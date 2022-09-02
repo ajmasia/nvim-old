0b3ca5700060d754c7181d5e0160f2ecd0d87290
Antonio José Masiá <email@antoniomasia.com>
2022-09-02 11:34:54 +0200

    add initial config to start using plugins

---
+++ init.lua
@@ -0,0 +1,5 @@
+require "user.plugins"
+require "user.keymaps"
+require "user.autocommands"
+require "user.options"
+require "user.colorscheme"
---
+++ lua/user/plugins.lua
@@ -0,0 +1,62 @@
+-- Plugins module
+
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  -- snapshot = "july-24",
+  snapshot_path = fn.stdpath "config" .. "/snapshots",
+  max_jobs = 50,
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+    prompt_border = "rounded", -- Border style of prompt popups.
+  },
+}
+
+-- Install your plugins here
+return packer.startup(function(use)
+  -- Plugin Mangager
+  use "wbthomason/packer.nvim" -- Have packer manage itself
+
+  -- colorschemas
+  use("lunarvim/colorschemes") -- Colorschemes bunch
+  use("navarasu/onedark.nvim") -- Onedark theme
+  use("folke/tokyonight.nvim") -- Tokynight theme
+
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
